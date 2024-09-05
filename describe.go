f397c20b44d5f2595543e7b93b37044d57559a82
孙琪(jade) <sunqi01@hypergryph.com>
2024-09-05 11:11:27 +0800

    p4 force describe

--- describe.go
+++ describe.go
@@ -42,6 +42,26 @@ func (conn *Conn) Describe(number uint64) (desc *Description, err error) {
 	return
 }
 
+func (conn *Conn) DescribeForce(number uint64) (desc *Description, err error) {
+	var (
+		results []Result
+	)
+	if results, err = conn.RunMarshaled("describe", []string{"-f", strconv.FormatUint(number, 10)}); err != nil {
+		return
+	}
+	for idx := range results {
+		if d, ok := results[idx].(*Description); !ok {
+			log.Printf("type translate err: %s", results[idx])
+			continue
+		} else {
+			// Get the first valid describe result and return it.
+			desc = d
+			return
+		}
+	}
+	return
+}
+
 func (conn *Conn) DescribeShelved(number uint64) (desc *Description, err error) {
 	var (
 		results []Result
